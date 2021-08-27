edb69c164f2b61d65b52b1f568dedff7e4da483e
pikagamed <51389229+pikagamed@users.noreply.github.com>
2021-08-27 08:55:50 +0800

    20210827
    Chessboard and event panel finish

---
+++ WuxingChess/Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    #region 欄位宣告
+        string name;
+        int score;
+        int panelTree;
+        int panelFire;
+        int panelEarth;
+        int panelMetal;
+        int panelWater;
+    #endregion
+
+    #region 屬性宣告
+    #endregion
+
+    #region 方法宣告
+    #endregion
+}
